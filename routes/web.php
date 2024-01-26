7b5139079fd92fb4e7ae6f79097d7d9a01f696a1
ReDMallard <marco@titori.it>
2024-01-26 03:20:20 +0100

    connette db table comics a home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ComicController;
+use App\Models\Comic;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $dati = config("data");
+    $dati = [
+        "comics" => Comic::all()
+    ];
     return view('home', $dati);
 })->name("home");
 
