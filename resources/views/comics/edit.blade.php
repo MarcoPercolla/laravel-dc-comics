fe3da307f9e82a630cf883325bf1f47b1add829f
ReDMallard <marco@titori.it>
2024-01-30 04:11:39 +0100

    copia per edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -4,7 +4,18 @@
 
 <div class="formContainer">
     
-    <h2>Nuovo fumetto</h2>
+    <h2>Modifica 
+        fumetto</h2>
+
+    @if ($errors->any())
+        <div class="alert">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     
     
     <form action="{{ route('comics.update', $comic->id) }}" method="POST">
@@ -13,39 +24,60 @@
         
         <div class="formLine">
             <label for="title" class="form-label">title</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{$comic->title}}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{$comic->title}}">
+            @error('title')
+                <div class="invalid-feedback">{{$message}}</div>
+            @enderror
         </div>
         <div class="formLine">
             <label for="description" class="form-label">description</label>
-            <textarea name="description" class="form-control" id="description"  rows="5">"{{$comic->description}}"</textarea>
+            <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description"  rows="5">"{{$comic->description}}"</textarea>
+            @error('description')
+                <div class="invalid-feedback">{{$message}}</div>
+            @enderror
         </div>
         <div class="formLine">
             <label for="thumb" class="form-label">thumb</label>
-            <input type="text" class="form-control" id="thumb" name="thumb" value="{{$comic->thumb}}">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{$comic->thumb}}">
+            @error('thumb')
+                <div class="invalid-feedback">{{$message}}</div>
+            @enderror
         </div>
         <div class="formLine">
             <label for="price" class="form-label">price</label>
-            <input type="text" class="form-control" id="price" name="price" value="{{$comic->price}}">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{$comic->price}}">
+            @error('price')
+                <div class="invalid-feedback">{{$message}}</div>
+            @enderror
         </div>
         <div class="formLine">
             <label for="series" class="form-label">series</label>
-            <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{$comic->series}}">
+            @error('series')
+                <div class="invalid-feedback">{{$message}}</div>
+            @enderror
         </div>
         <div class="formLine">
             <label for="sale_date" class="form-label">sale_date</label>
-            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+            <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+            @error('sale_date')
+                <div class="invalid-feedback">{{$message}}</div>
+            @enderror
         </div>
         <div class="formLine">
             <label for="type" class="form-label">type</label>
-            <input type="text" class="form-control" id="type" name="type" value="{{$comic->type}}">
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value="{{$comic->type}}">
+            @error('type')
+                <div class="invalid-feedback">{{$message}}</div>
+            @enderror
         </div>
-        <button type="submit">Modifica</button>
+        <button class="btn" type="submit">Modifica</button>
     </form>
 
     <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
         @csrf
         @method('DELETE')
-        <input type="submit" value="Cancella" >
+        <input class="btnDelete" type="submit" value="Cancella" >
     </form>
     
 </div>
